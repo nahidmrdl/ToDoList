d83e0ae0a7e690059a313073749ce7268e16602d
nahidmrdl <muradlinahid2003@gmail.com>
2024-06-12 02:43:35 +0200

    Add project files.

---
+++ To Do List App/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace To_Do_List_App
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.Description = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.newButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dueDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.priorityBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1061, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do List";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(12, 73);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(1061, 20);
+            this.textBoxTitle.TabIndex = 1;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(12, 130);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(1061, 20);
+            this.textBoxDescription.TabIndex = 2;
+            // 
+            // Description
+            // 
+            this.Description.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Description.Location = new System.Drawing.Point(12, 104);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(1061, 23);
+            this.Description.TabIndex = 3;
+            this.Description.Text = "Description";
+            // 
+            // Title
+            // 
+            this.Title.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(12, 47);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(1061, 23);
+            this.Title.TabIndex = 4;
+            this.Title.Text = "Title";
+            // 
+            // newButton
+            // 
+            this.newButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newButton.Location = new System.Drawing.Point(12, 237);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(254, 31);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "New (Clear Fields)";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editButton.Location = new System.Drawing.Point(281, 237);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(254, 31);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.Location = new System.Drawing.Point(550, 237);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(254, 31);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(819, 237);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(254, 31);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.BackgroundColor = System.Drawing.Color.White;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.Location = new System.Drawing.Point(12, 274);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.Size = new System.Drawing.Size(1061, 429);
+            this.toDoListView.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Due Date";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(281, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(254, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Priority Level";
+            // 
+            // dueDatePicker
+            // 
+            this.dueDatePicker.CalendarFont = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dueDatePicker.Location = new System.Drawing.Point(12, 192);
+            this.dueDatePicker.Name = "dueDatePicker";
+            this.dueDatePicker.Size = new System.Drawing.Size(254, 20);
+            this.dueDatePicker.TabIndex = 12;
+            // 
+            // priorityBox
+            // 
+            this.priorityBox.FormattingEnabled = true;
+            this.priorityBox.Location = new System.Drawing.Point(281, 191);
+            this.priorityBox.Name = "priorityBox";
+            this.priorityBox.Size = new System.Drawing.Size(254, 21);
+            this.priorityBox.TabIndex = 13;
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1085, 715);
+            this.Controls.Add(this.priorityBox);
+            this.Controls.Add(this.dueDatePicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "ToDoList";
+            this.Text = "To Do List";
+            this.Load += new System.EventHandler(this.ToDoList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView toDoListView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dueDatePicker;
+        private System.Windows.Forms.ComboBox priorityBox;
+    }
+}
+
---
+++ To Do List App/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace To_Do_List_App
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        DataTable toDoList = new DataTable();
+        bool isEditing = false;
+
+        private void ToDoList_Load(object sender, EventArgs e)
+        {
+            // Create columns
+            toDoList.Columns.Add("Title");
+            toDoList.Columns.Add("Description");
+            toDoList.Columns.Add("Due Date");
+            toDoList.Columns.Add("Priority"); 
+
+            toDoListView.DataSource = toDoList;
+
+            // Add items to the priorityBox
+            priorityBox.Items.AddRange(new string[] { "High", "Medium", "Low" });
+            priorityBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBoxTitle.Text = "";
+            textBoxDescription.Text = "";
+            dueDatePicker.Value = DateTime.Now;
+            priorityBox.SelectedIndex = -1;
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            if (toDoListView.CurrentRow != null)
+            {
+                isEditing = true;
+
+                // Fill text fields with data from table
+                textBoxTitle.Text = toDoList.Rows[toDoListView.CurrentRow.Index]["Title"].ToString();
+                textBoxDescription.Text = toDoList.Rows[toDoListView.CurrentRow.Index]["Description"].ToString();
+                dueDatePicker.Value = DateTime.Parse(toDoList.Rows[toDoListView.CurrentRow.Index]["Due Date"].ToString());
+                priorityBox.SelectedItem = toDoList.Rows[toDoListView.CurrentRow.Index]["Priority"].ToString();
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (toDoListView.CurrentRow != null)
+            {
+                var confirmResult = MessageBox.Show("Are you sure to delete this item?", "Confirm Delete", MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+                    toDoList.Rows[toDoListView.CurrentCell.RowIndex].Delete();
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                if (isEditing)
+                {
+                    toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Title"] = textBoxTitle.Text;
+                    toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Description"] = textBoxDescription.Text;
+                    toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Due Date"] = dueDatePicker.Value.ToString("yyyy-MM-dd");
+                    toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Priority"] = priorityBox.SelectedItem.ToString();
+                }
+                else
+                {
+                    toDoList.Rows.Add(textBoxTitle.Text, textBoxDescription.Text, dueDatePicker.Value.ToString("yyyy-MM-dd"), priorityBox.SelectedItem.ToString());
+                }
+                ClearTextBoxes();
+                isEditing = false;
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxTitle.Text))
+            {
+                MessageBox.Show("Title cannot be empty.");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxDescription.Text))
+            {
+                MessageBox.Show("Description cannot be empty.");
+                return false;
+            }
+            if (priorityBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a priority level.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
